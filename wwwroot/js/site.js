5566b25c1ddfdd9a121e2494a5880f150f7cf1b6
James Mcdonald <jamesmcdonald661@gmail.com>
2023-11-22 13:02:53 -0400

    commit

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,49 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', function() {
+    const addForm = document.getElementById('addQuoteForm');
+    const deleteButton = document.getElementById('deleteQuoteButton');
+    const deleteSelect = document.getElementById('deleteQuoteSelect');
 
-// Write your JavaScript code.
+    // Handle Add Quote Form Submission
+    addForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const formData = new FormData(addForm);
+        // Add logic to handle file upload if necessary
+
+        fetch('/api/quotes', {
+            method: 'POST',
+            body: JSON.stringify({
+                author: formData.get('author'),
+                quoteText: formData.get('quoteText'),
+                permalink: formData.get('permalink') // Handle optional permalink
+                // Add image handling if necessary
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                // Handle successful addition
+                window.location.reload(); // Reload page to show new quote
+            } else {
+                // Handle error
+                alert('Error adding quote');
+            }
+        });
+    });
+
+    // Handle Delete Quote
+    deleteButton.addEventListener('click', function() {
+        const selectedQuoteId = deleteSelect.value;
+        fetch(`/api/quotes/${selectedQuoteId}`, {
+            method: 'DELETE'
+        }).then(response => {
+            if (response.ok) {
+                // Handle successful deletion
+                window.location.reload(); // Reload page to update list
+            } else {
+                // Handle error
+                alert('Error deleting quote');
+            }
+        });
+    });
+});
